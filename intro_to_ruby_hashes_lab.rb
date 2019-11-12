8b43258c92ab27bab80bb5f821839c1b9754a932
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-12 20:39:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,24 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    name: "flash"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ pioneer = {
+   :name => "Grace Hopper"
+ }
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    :id => 6
+  }
+  id_generator[:id]
 end
 
 def my_hash_creator(key, value)
