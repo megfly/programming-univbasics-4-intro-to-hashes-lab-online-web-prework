218622ebaafbc9d375efcf1c53420696dd25d361
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-12 21:55:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,10 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash = {
+    name => :Steve
+    "name" => :Tzvi
+  }
 end
 
 def update_counting_hash(hash, key)
